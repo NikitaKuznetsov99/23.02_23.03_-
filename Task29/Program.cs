59f62af49d3c1cb945f0becb3ab1d7a3d47c983f
Nikita <nikita.kuznecov.99@bk.ru>
2023-03-14 04:08:06 +0700

    Изменил задачу 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,13 +3,17 @@
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
 
-Console.WriteLine("Введите размерность массива");
-int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите размерность массива");
+// int N = Convert.ToInt32(Console.ReadLine());
 
-int[] array = new int[N];
+// int[] array = new int[N];
 
-for (int i = 0;i<N;i++)
-    array[i] = Convert.ToInt32(Console.ReadLine());
+// for (int i = 0; i < N; i++)
+//     array[i] = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i < N; i++)
-    Console.Write(array[i] + " ");
+// for (int i = 0; i < N; i++)
+//     Console.Write(array[i] + " ");
+
+int[] a = Enumerable.Range(1, 8).Select(x => 17 % x).ToArray(); 
+foreach (int i in a)
+  Console.WriteLine(i);
