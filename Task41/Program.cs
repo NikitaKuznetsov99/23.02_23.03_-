d0419aba4d138c14bdc51654c965290666da075e
Nikita <nikita.kuznecov.99@bk.ru>
2023-04-03 18:21:20 +0700

    Добавил задачи 41 и 43

---
+++ Task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+???
+// using System;
+// class HelloWorld
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Введите M");
+//         int M = Convert.ToInt32(Console.ReadLine());
+
+//         int count = 0;
+//         int inp = 0;
+//         for (int i = 1; i <= M; i++)
+//         {
+//             inp = Convert.ToInt32(Console.ReadLine());
+//             if (inp > 0)
+//                 count++;
+//         }
+
+//         Console.WriteLine(count);
+//         Console.ReadLine();
+//     }
+// }
---
+++ Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using System;
+class HelloWorld {
+static void Main() {
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+}
+}
